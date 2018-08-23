a9a7a991721f0c0ca1ad4ac2929bddaa852dfde3
CleverNamesTaken <david.k.lee3@gmail.com>
2018-08-22 21:43:52 -0400

    Fixed for bash scripts

--- scripts/echo-ify.sh
+++ scripts/echo-ify.sh
@@ -5,13 +5,13 @@ if [ $# -eq 0 ]; then
 	echo '[-] Example: .\script.sh test.vbs'
 	exit 0
 fi
-echo '[*] The echo-fied version of the script is as follows:'
-echo ''
-echo ''
-sedCommand="sed -e 's/^/echo \'/g; s/$/\' >> "$1" \&/g ' $1 |tr '\n' ' ' | sed 's/...$//'"
+#echo '[*] The echo-fied version of the script is as follows:'
+#echo ''
+#echo ''
+sedCommand="sed -e \"s/^/echo '/g; s/\$/\' >> $1 \&\&/g \" $1 |tr '\n' ' ' | sed 's/...$//'"
 eval $sedCommand
 echo ''
 echo ''
 echo "[*] This command will be saved in $1_echofied."
-sedCommand="sed -e 's/^/echo \'/g; s/$/\' >> "$1" \&/g ' $1 |tr '\n' ' ' | sed 's/...$//' > $1_echofied"
+sedCommand="sed -e \"s/^/echo '/g; s/\$/\' >> $1 \&\&/g \" $1 |tr '\n' ' ' | sed 's/...$//' > $1_echofied"
 eval $sedCommand
